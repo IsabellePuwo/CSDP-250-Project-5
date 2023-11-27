bb3f443ba87b7ec1a20458527c06ee9bf4e23fee
Isabelle Puwo <126019713+IsabellePuwo@users.noreply.github.com>
2023-11-27 15:33:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,37 +12,48 @@ int main(){
     BST tree;
     const int numNumbers = 35;
     
-    tree.generateRandomNumbers(numNumbers);
-    cout << "Inorder Traversal: \n";
-    tree.inorderTraversal();
-    cout<< "\n\n";
-    cout<<"Do you want to delete a node from the tree? (y/n): ";
-    char choice;
-    cin>> choice;
-    
-    if (choice == 'y') {
-            int value;
-            cout << "Enter the value to delete: ";
-            cin >> value;
-            tree.removeNode(value);
-            cout << "Postorder Traversal: \n";
-            tree.postOrderTraversal();
-            cout << "\n\n";
+   tree.generateRandomNumbers(numNumbers);
+    while (true) {
+           cout << "Menu:\n";
+           cout << "1. Display Inorder Traversal\n";
+           cout << "2. Delete a Node\n";
+           cout << "3. Check if it's an AVL Tree\n";
+           cout << "4. Exit\n";
+           cout << "Enter your choice (1-4): ";
 
-            if (tree.checkifAVL()) {
-                cout << "The tree is an AVL Tree.\n";
-            } else {
-                cout << "The tree is not an AVL Tree.\n";
-            }
-        } else if (choice == 'n') {
-            cout << "Ending the program...\n";
-            exit(0);
-        } else {
-            cout << "Invalid input. Ending the program.\n";
-        }
+           int choice;
+           cin >> choice;
 
-        cout << "Program ended successfully.\n";
-        return 0;
-    
+           switch (choice) {
+               case 1:
+                   cout << "Inorder Traversal: \n";
+                   tree.inorderTraversal();
+                   cout << "\n\n";
+                   break;
+               case 2:
+                   cout << "Enter the value to delete: ";
+                   int value;
+                   cin >> value;
+                   tree.removeNode(value);
+                   cout << "Postorder Traversal: \n";
+                   tree.postOrderTraversal();
+                   cout << "\n\n";
+                   break;
+               case 3:
+                   if (tree.checkifAVL()) {
+                       cout << "The tree is an AVL Tree.\n";
+                   } else {
+                       cout << "The tree is not an AVL Tree.\n";
+                   }
+                   break;
+               case 4:
+                   cout << "Ending the program...\n";
+                   exit(0);
+               default:
+                   cout << "Invalid input. Please enter a number between 1 and 5.\n";
+           }
+       }
+
+       return 0;
     
 }
